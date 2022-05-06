834acb30892f7859b9d7c08d46c09cab71818adf
Morigor <morozigor@ukr.net>
2022-05-06 18:23:01 +0300

    changed one variable in incoming array in testSumOfTwoBug

--- src/test/java/SumOfTwoTest.java
+++ src/test/java/SumOfTwoTest.java
@@ -28,7 +28,7 @@ public class SumOfTwoTest {
 
     @Test
     public void testSumOfTwoBug() {
-        int[] numbers = {12, 4, -3, -7, 0, -5, 2, 9, 3, 12};
+        int[] numbers = {12, 4, -3, -7, 0, -5, 1, 9, 3, 12};
         int n = 12;
         int[][] expectedResult = {{12, 0}, {0, 12}, {9, 3}};  //  средняя пара не должна быть
         SumOfTwo obj = new SumOfTwo();
